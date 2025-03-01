52db009e3c32ed429f52b43035882e9adf2b3845
Slatu <138379901+dSlatu@users.noreply.github.com>
2025-03-01 21:23:57 +0100

    Update fxmanifest.lua

--- Slatu_inventoryOX/fxmanifest.lua
+++ Slatu_inventoryOX/fxmanifest.lua
@@ -1,11 +1,13 @@
 fx_version 'cerulean'
 game 'gta5'
 
+author 'Slatu'
+
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
 
-    'miradouro_inventory.lua',
+    'slatu_inventory.lua',
 
 }
 
